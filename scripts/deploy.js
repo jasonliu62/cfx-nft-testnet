eb29128faea3b3caffb88f9803aeac49d142199e
Jason Liu <jasondev310@gmail.com>
2024-06-26 17:48:40 +0800

    show balance

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,7 @@ const { ethers } = require("hardhat");
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const MyContract = await ethers.getContractFactory("MyContract");
     const myContract = await MyContract.deploy();
